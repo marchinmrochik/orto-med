c4a8fa451c2106c796c600555bd38aaa9290ed38
liza <lizatsaryova@gmail.com>
2019-06-16 16:08:38 +0200

    fixed select phone

--- static/js/select.js
+++ static/js/select.js
@@ -1,3 +1,7 @@
 $("#select-phone").selectpicker({
     showSubtext: true
 });
+
+$('#select-phone').on('changed.bs.select', function (e, clickedIndex, isSelected, previousValue) {
+    window.location.href =  e.currentTarget[clickedIndex].value
+});
