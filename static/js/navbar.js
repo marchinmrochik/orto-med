8b2def47f2dbf18535eb39c0ef1bc13df6a05859
liza <lizatsaryova@gmail.com>
2019-06-16 16:23:41 +0200

    fixed tabs

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,3 +1,12 @@
 $('.nav-item[data-toggle="tab"]').on('shown.bs.tab', function (e) {
     $('#nav-tab_content').addClass('show_block');
 });
+
+$(document).mouseup(function (e){ // событие клика по веб-документу
+    var div = $("#header"); // тут указываем ID элемента
+    if (!div.is(e.target) // если клик был не по нашему блоку
+        && div.has(e.target).length === 0) { // и не по его дочерним элементам
+        $('#nav-tab_content').removeClass('show_block'); // скрываем его
+        $('.nav-item[data-toggle="tab"]').hasClass('active').removeClass('show_block').setAttribute('aria-checked', 'false');
+    }
+});
