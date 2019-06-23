29535f38f49bc6d85b2667461a09294749ca5b6c
liza <lizatsaryova@gmail.com>
2019-06-23 17:16:55 +0200

    added lightbox

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -1,4 +1,8 @@
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
-    $(this).ekkoLightbox({ wrapping: false });
+    $(this).ekkoLightbox({
+        wrapping: false,
+        alwaysShowClose: true,
+        showArrows: false
+    });
 });
