376aceed24daab83f224ba9cbbe9cc65f1a984c5
liza <lizatsaryova@gmail.com>
2019-06-26 17:08:57 +0200

    fix text in header and added link

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -2,7 +2,6 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox({
         wrapping: false,
-        alwaysShowClose: true,
-        showArrows: false
+        alwaysShowClose: true
     });
 });
