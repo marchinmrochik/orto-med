2a707471da8cf847fb1a561fc4c1b49d4da42b8c
liza <lizatsaryova@gmail.com>
2019-07-02 17:19:44 +0200

    added page

--- static/js/select.js
+++ static/js/select.js
@@ -9,3 +9,10 @@ $('#select-phone').on('changed.bs.select', function (e, clickedIndex, isSelected
 $('#select-modification').selectpicker({});
 
 $('#select-quantity').selectpicker({});
+
+
+$('#select-delivery').selectpicker({});
+
+$('#select-delivery').on('changed.bs.select', function (e, clickedIndex, isSelected, previousValue) {
+    clickedIndex == 0 ? ($('.block-delivery-courier').removeClass('d-none'), $('.block-delivery-department').addClass('d-none')) : ($('.block-delivery-department').removeClass('d-none'), $('.block-delivery-courier').addClass('d-none'))
+});
