2f3c894433acc3800787b9c1c21e63938d3b10ff
liza <lizatsaryova@gmail.com>
2019-07-09 04:26:05 +0200

    small fix accordions

--- static/js/accordion.js
+++ static/js/accordion.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     location.hash && $(location.hash + '.collapse').collapse('show');
 });
 
+$('.block-item-menu .nav-item.nav-link').on('click', () => location.reload());
+
 $('.panel-collapse').on('show.bs.collapse', function () {
     $('.panel.panel-default').find('.active').removeClass('active').siblings('.show').removeClass('show');
     $(this).siblings('.panel-heading').addClass('active');
